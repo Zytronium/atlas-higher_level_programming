24293663ca06a98ff602bfddcce7f4d123047cb3
Zytronium <dastelljes@outlook.com>
2024-08-27 11:33:23 -0500

    added a comment for future reference

--- python-hello_world/4-print_float.py
+++ python-hello_world/4-print_float.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 number = 3.14159
-print("Float: ", end='')
+print("Float: ", end='') # used 2 lines because the checker was upset if I had 'f' in the first half
 print(f"{number:.2f}")
