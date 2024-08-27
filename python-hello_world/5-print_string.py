09ce0b20d6bf8a9ec41d48cd75bcf472e8c35dbe
Zytronium <dastelljes@outlook.com>
2024-08-27 11:43:33 -0500

    removed comment that made the line too long

--- python-hello_world/5-print_string.py
+++ python-hello_world/5-print_string.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 str = "Holberton School"
-print(str * 3 + '\n' + str[:9]) # They said I can use up to 5 lines, but I knew I could do it in less, so I did
+print(str * 3 + '\n' + str[:9])
