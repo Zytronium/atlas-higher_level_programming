0677927d76157736f49a1e0aa3a80050a6d800df
Zytronium <dastelljes@outlook.com>
2024-08-28 14:31:11 -0500

    finished task 1 why does git detect a change in 0-positive_or_negative.py when there is none

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-print(f"Last digit of {number} is {number[-1:]} and is ", end='')
-if number[-1:] > 5:
+lastDigit = int(str(number)[-1:])
+print(f"Last digit of {number} is {lastDigit} and is ", end='')
+if lastDigit > 5:
     print("greater than 5")
-elif number[-1:] == 0:
+elif lastDigit == 0:
     print("0")
 else:
     print("less than 6 and not 0")
