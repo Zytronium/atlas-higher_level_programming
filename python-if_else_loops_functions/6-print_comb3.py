69b70c4867aa64ddfae43701adb88721f5bb22f8
root <dastelljes@outlook.com>
2024-08-30 20:36:55 +0000

    did task 6

---
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for i in range(10):
+    for j in range(10):
+        if i < j and not (i == 8 and j == 9):
+            print(f"{i}{j}", end=', ')
+        elif i == 8 and j == 9:
+            print(89)
