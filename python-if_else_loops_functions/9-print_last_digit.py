d7765271e5062c26b9b8ad11c9cc35180fded74c
Zytronium <dastelljes@outlook.com>
2024-08-30 16:29:45 -0500

    finished task 9

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    print("{n}".format(n = int(str(number)[-1:])), end='')
-
-
-print_last_digit(98)
-print_last_digit(0)
-r = print_last_digit(-1024)
-print(r)
+    last_digit = int(str(number)[-1:])
+    print("{n}".format(n = last_digit), end='')
+    return last_digit
