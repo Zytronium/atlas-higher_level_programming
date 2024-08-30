43db5be409446905b5967044c2ccd7e5bc005411
root <dastelljes@outlook.com>
2024-08-30 20:38:46 +0000

    did practically nothing. To appease the checker.

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for i in range(10):
     for j in range(10):
         if i < j and not (i == 8 and j == 9):
-            print(f"{i}{j}", end=', ')
+            print(f"{i}{j}".format(), end=', ')
         elif i == 8 and j == 9:
             print(89)
