0f935dd2c633c87246afa62b2273c99ced435522
Zytronium <dastelljes@outlook.com>
2024-08-30 15:56:44 -0500

    formatting correction

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-    if c == '': # appease the checker by faking an error
+    if c == '':  # appease the checker by faking an error
         print("Traceback (most recent call last):")
         exit()
     return True if 'a' <= c <= 'z' else False
