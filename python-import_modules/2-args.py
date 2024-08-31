e0fbd7c9ed50702cbad443f1a569d9ddce329c6b
Zytronium <dastelljes@outlook.com>
2024-08-31 13:39:35 -0500

    did task 2

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+from sys import argv
+
+if __name__ == "__main__":
+    argc = len(argv) - 1
+    print("{n} argument{s}.".format(n=argc, s='s' if argc != 1 else ''))
+    if argc != 0:
+        for i in range(1, argc + 1):
+            print("{n}: {a}".format(n=i, a=argv[i]))
