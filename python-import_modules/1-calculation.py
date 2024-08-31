aec426fcfd85cb393e3a100fa09cd425a8cde64a
Zytronium <dastelljes@outlook.com>
2024-08-31 13:03:20 -0500

    finished task 1

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import calculator_1
+    from calculator_1 import add, sub, mul, div
     a = 10
     b = 5
+    print("{a} + {b} = {c}".format(a=a, b=b, c=add(a, b)))
+    print("{a} - {b} = {c}".format(a=a, b=b, c=sub(a, b)))
+    print("{a} * {b} = {c}".format(a=a, b=b, c=mul(a, b)))
+    print("{a} / {b} = {c}".format(a=a, b=b, c=div(a, b)))
