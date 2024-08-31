d8cb62a634014b2305307da8b4606133face1cc1
Zytronium <dastelljes@outlook.com>
2024-08-31 13:47:01 -0500

    formatting

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,7 +3,8 @@ from sys import argv
 
 if __name__ == "__main__":
     argc = len(argv) - 1
-    print("{n} argument{s}{punct}".format(n=argc, s='s' if argc != 1 else '', punct=':' if argc != 0 else '.'))
+    print("{n} argument{s}{p}".format(
+        n=argc, s='s' if argc != 1 else '', p=':' if argc != 0 else '.'))
     if argc != 0:
         for i in range(1, argc + 1):
             print("{n}: {a}".format(n=i, a=argv[i]))
