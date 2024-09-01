fe6b2602aabf71f8a22f8e5d4fd8e0ab99a5a994
Zytronium <dastelljes@outlook.com>
2024-09-01 15:31:58 -0500

    fixed task 5

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
-def no_c(my_string : str):
-    return my_string.join(char for char in my_string if char != 'c' and char != 'C')
-
-# print(no_c("Best School"))
-# print(no_c("Chicago"))
-# print(no_c("C is fun!"))
+def no_c(my_string):
+    return ''.join(char for char in my_string if char != 'c' and char != 'C')
