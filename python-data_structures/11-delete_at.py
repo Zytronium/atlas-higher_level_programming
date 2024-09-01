8ad3241a9f9ac5cf0cb0e62b1b8410ff90a0713e
Zytronium <dastelljes@outlook.com>
2024-09-01 16:24:20 -0500

    prgress on task 11

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
     if 0 < idx < len(my_list):
-        my_list.pop(idx)
+        new_list = []
+        for i in range(len(my_list)):
+            if i != idx:
+                new_list.append(my_list[i])
+        return new_list
     return my_list
 
 my_list = [1, 2, 3, 4, 5]
