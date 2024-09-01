68b1fc983c61308ca9642c21c5596ceb89d4e611
Zytronium <dastelljes@outlook.com>
2024-09-01 16:13:14 -0500

    finished task 8

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    print("Length: {:d} - First character: {}".format(
-        len(sentence), sentence[0] if len(sentence) > 0 else None))
+    return len(sentence), sentence[0] if len(sentence) > 0 else None
