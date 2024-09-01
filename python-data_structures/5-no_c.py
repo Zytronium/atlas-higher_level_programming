9b5e37b62e2171df3dfd423c32ccc3692a8a6b5d
Zytronium <dastelljes@outlook.com>
2024-09-01 15:32:43 -0500

    slightly simplified task 5

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    return ''.join(char for char in my_string if char != 'c' and char != 'C')
+    return ''.join(char for char in my_string if char not in 'cC')
