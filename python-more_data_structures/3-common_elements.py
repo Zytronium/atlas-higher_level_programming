a9bb78cec7796a7fa04731aace6f8718cb03f677
Zytronium <dastelljes@outlook.com>
2024-09-02 10:13:12 -0500

    Realized a mistake and simplified task 3 I realized I was actually using a typedDict by declaring common_items at "{}" instead of "set()" or "set({})"

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    common_items = {}
+    common_items = set()
     for item in set_1:
         if item in set_2:
-            common_items[item] = common_items.get(item, 0) + 1
+            common_items.add(item)
     return common_items
