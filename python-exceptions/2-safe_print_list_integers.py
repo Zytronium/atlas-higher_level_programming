303310779c0a80e2d4b39d1c679b62d229436820
Zytronium <dastelljes@outlook.com>
2024-09-02 18:49:24 -0500

    fixed task 2 by deleting test code

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -2,7 +2,6 @@
 
 def safe_print_list_integers(my_list=[], x=0):
     n = 0
-
     for i in range(0, x):
         try:
             print("{:d}".format(my_list[i]), end="")
@@ -13,15 +12,3 @@ def safe_print_list_integers(my_list=[], x=0):
             pass
     print()
     return n
-
-my_list = [1, 2, 3, 4, 5]
-
-nb_print = safe_print_list_integers(my_list, 2)
-print("nb_print: {:d}".format(nb_print))
-
-my_list = [1, 2, 3, "School", 4, 5, [1, 2, 3]]
-nb_print = safe_print_list_integers(my_list, len(my_list))
-print("nb_print: {:d}".format(nb_print))
-
-nb_print = safe_print_list_integers(my_list, len(my_list) + 13)
-print("nb_print: {:d}".format(nb_print))
