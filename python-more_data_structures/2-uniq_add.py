874a0654b4b5abb0f43e3d3fc565f490bd0b085a
Zytronium <dastelljes@outlook.com>
2024-09-02 09:28:49 -0500

    fixed task 2 by removing debug print

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -4,7 +4,6 @@ def uniq_add(my_list=[]):
     sorted_numbers.sort()  # Could also iterate through to get the unique numbs
     total = 0
     for i in range(len(sorted_numbers)):
-        print("{:d}".format(i))
         if i > 0 and sorted_numbers[i] != sorted_numbers[i-1]:
             total += sorted_numbers[i]
         elif i == 0:
