23c589bfa5afb0402569bb5b2f4cb31e31f11cf0
Zytronium <dastelljes@outlook.com>
2024-09-02 18:48:38 -0500

    fixed task 2 by allowing an index error and chaning some other stuff

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,25 +1,18 @@
 #!/usr/bin/python3
-from traceback import TracebackException
-
 
 def safe_print_list_integers(my_list=[], x=0):
     n = 0
-    i = 0
 
-    for item in my_list:
-        if i < x:
-            try:
-                print("{:d}".format(item), end='')
-                n += 1
-            except IndexError:
-                print("Index Error")
-            except ValueError:
-                pass
-            except TypeError:
-                pass
-            i += 1
+    for i in range(0, x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            n += 1
+        except ValueError:
+            pass
+        except TypeError:
+            pass
     print()
-    return i
+    return n
 
 my_list = [1, 2, 3, 4, 5]
 
@@ -30,5 +23,5 @@ my_list = [1, 2, 3, "School", 4, 5, [1, 2, 3]]
 nb_print = safe_print_list_integers(my_list, len(my_list))
 print("nb_print: {:d}".format(nb_print))
 
-nb_print = safe_print_list_integers(my_list, len(my_list) + 3)
+nb_print = safe_print_list_integers(my_list, len(my_list) + 13)
 print("nb_print: {:d}".format(nb_print))
