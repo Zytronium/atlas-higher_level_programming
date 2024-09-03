5af7611239c6160466fa4da9ee15965c78268d91
Zytronium <dastelljes@outlook.com>
2024-09-02 19:00:02 -0500

    fixed task 3

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -6,5 +6,5 @@ def safe_print_division(a, b):
     except ZeroDivisionError:
         return None
     finally:
-        print("{}".format(c))
+        print("Inside result: {}".format(c))
         return c
