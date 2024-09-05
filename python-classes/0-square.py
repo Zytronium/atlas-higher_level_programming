1c4aa6f5946db7171e2343484014656465a5aba5
Zytronium <dastelljes@outlook.com>
2024-09-05 15:32:51 -0500

    fixed task 0 by adding documentation

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
 class Square:
+    """
+    just a square with no attributes
+    """
     def __init__(self):
+        """
+        empty init method
+        """
         pass
