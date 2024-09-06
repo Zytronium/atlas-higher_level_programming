6c5f51da02f134898525411cf319f39c583e1d3c
Zytronium <dastelljes@outlook.com>
2024-09-06 17:30:15 -0500

    started task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+# task 1
+"""
+
+
+class Square:
+    """
+    # a square
+    """
+    def __init__(self, size):
+        """
+        # the init method
+        :param size: the size of the square
+        """
+        self.__size = size
