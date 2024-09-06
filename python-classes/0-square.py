6d44f092e61301318028272f37b328dcf3a48ae3
Zytronium <dastelljes@outlook.com>
2024-09-06 17:17:50 -0500

    attempt to comply with documentation based on what the checker output says

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,17 +1,15 @@
-#
-
 #!/usr/bin/python3
 """
-0-square is an empty class
+# 0-square is an empty class
 """
 
 
 class Square:
     """
-    just a square with no attributes
+    # just a square with no attributes
     """
     def __init__(self):
         """
-        empty init method
+        # empty init method
         """
         pass
