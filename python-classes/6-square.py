b3ecf581acb87e1124bd6d1f1341828b32db8b45
Zytronium <dastelljes@outlook.com>
2024-09-07 16:15:58 -0500

    fixed task 6 again wifi went out at the worst time. using mobile hotspot

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -27,6 +27,7 @@ class Square:
             raise ValueError("size must be >= 0")
 
         if (type(position) is not tuple or len(position) != 2
+                or type(position[0]) is not int or type(position[1]) is not int
                 or position[0] < 0 or position[1] < 0):
             raise TypeError("position must be a tuple of 2 positive integers")
 
@@ -82,6 +83,7 @@ class Square:
         :param value: the new position of the square
         """
         if (type(value) is not tuple or len(value) != 2
-                or value[0] < 0 or value[1] < 0):
+            or type(value[0]) is not int or type(value[1]) is not int
+            or value[0] < 0 or value[1] < 0):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
