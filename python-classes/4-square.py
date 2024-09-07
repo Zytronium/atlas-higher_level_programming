a841605caebd37620d0cddc127193323aac73f48
Zytronium <dastelljes@outlook.com>
2024-09-07 15:19:34 -0500

    did task 4

---
+++ python-classes/4-square.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""
+# task 4
+"""
+
+
+class Square:
+    """
+    # a square
+    """
+    def __init__(self, size=0):
+        """
+        # init method
+        :param size: the size of the square. Must be a non-negative integer
+        """
+        self.__size = size
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if self.__size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """
+        # returns the area of the square based on its size
+        :return: the area of the square
+        """
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        """
+        retrieves the size of the square
+        :return: the size of the square
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        sets the size of the square
+        :param value: the new size of the square
+        """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
