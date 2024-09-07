2d4f6ef4ce757ba9b320075d6cdfbac2d1f92ec4
Zytronium <dastelljes@outlook.com>
2024-09-07 15:30:38 -0500

    started task 6

---
+++ python-classes/6-square.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+"""
+task 6
+"""
+
+
+class Square:
+    """
+    # a square
+    """
+    def __init__(self, size=0, position=(0, 0)):
+        """
+        init method
+        :param size: the size of the square. Must be a non-negative integer
+        """
+        self.__size = size
+        self.__position = position
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if self.__size < 0:
+            raise ValueError("size must be >= 0")
+
+        if type(position) is not tuple or len(position) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+
+    def area(self):
+        """
+        returns the area of the square based on its size
+        :return: the area of the square
+        """
+        return self.__size * self.__size
+
+    def my_print(self):
+        """
+        prints the square using the '#' character
+        """
+        for i in range(self.__size):
+            print("#" * self.__size)
+        if self.__size == 0:
+            print()  # still print something if size is 0
+
+    @property
+    def size(self):
+        """
+        retrieves the size of the square
+        :return: the size of the square
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        sets the size of the square
+        :param value: the new size of the square
+        """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
