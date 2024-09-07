af5b7588871bc815bd067bbfaa755dd7b4a04301
Zytronium <dastelljes@outlook.com>
2024-09-07 15:08:52 -0500

    did task 2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -11,6 +11,43 @@ class Square:
     def __init__(self, size=0):
         """
         # init method
-        :param size: the size of the square
+        :param size: the size of the square. Must be a non-negative integer
         """
         self.__size = size
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if self.__size < 0:
+            raise ValueError("size must be >= 0")
+
+
+my_square_1 = Square(3)
+print(type(my_square_1))
+print(my_square_1.__dict__)
+
+my_square_2 = Square()
+print(type(my_square_2))
+print(my_square_2.__dict__)
+
+try:
+    print(my_square_1.size)
+except Exception as e:
+    print(e)
+
+try:
+    print(my_square_1.__size)
+except Exception as e:
+    print(e)
+
+try:
+    my_square_3 = Square("3")
+    print(type(my_square_3))
+    print(my_square_3.__dict__)
+except Exception as e:
+    print(e)
+
+try:
+    my_square_4 = Square(-89)
+    print(type(my_square_4))
+    print(my_square_4.__dict__)
+except Exception as e:
+    print(e)
