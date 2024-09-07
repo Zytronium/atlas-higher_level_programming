64026ba102b5f4fd38b69a7b330b63db9eeba848
Zytronium <dastelljes@outlook.com>
2024-09-07 16:18:23 -0500

    attempted to fix task 6 again

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -42,7 +42,8 @@ class Square:
         """
         prints the square using the '#' character
         """
-        print("\n" * self.__position[1], end='')
+        if self.__position[1] > 0:
+            print()
         for i in range(self.__size):
             print(" " * self.__position[0] + "#" * self.__size)
         if self.__size == 0:
