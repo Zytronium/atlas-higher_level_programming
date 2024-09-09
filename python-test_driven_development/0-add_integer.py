c08eb18b3d5ca3b92272c135000e97e09a0376d9
Zytronium <dastelljes@outlook.com>
2024-09-09 13:10:13 -0500

    did task 0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""
+    This module, from task 0, contains a function that adds 2 integers.
+"""
+
+
+def add_integer(a, b=98):  # normally we would probably rather do b=0
+    """
+    Adds integers a and b and returns the result
+    :param a: first integer
+    :param b: second integer. B = 98 if not specified
+    :return: the sum of a + b
+    """
+    return a + b
