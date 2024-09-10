fe600aa852b6c0a3efb914efc3705c6a79b1238b
Zytronium <dastelljes@outlook.com>
2024-09-10 13:04:10 -0500

    started task 5

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -7,19 +7,9 @@ def text_indentation(text):
     if type(text) is not str:
         raise TypeError("text must be a string")
     
-    print(text.replace(".", ".\n\n").replace(",", ",\n\n").replace(
-        "?", "?\n\n").replace(":", ":\n\n").replace(" \n", "\n").replace(
-        "\n ", "\n"), end='')
-
-if __name__ == "__main__":
-    text_indentation("Never gonna. Give, you: up? Never .  Gonna let you,,, down. . .?")
-    print("\n----------------\n")
-    text_indentation("""Lorem ipsum dolor sit amet, consectetur adipiscing elit. \
-Quonam modo? Utrum igitur tibi litteram videor an totas paginas commovere? \
-Non autem hoc: igitur ne illud quidem. Fortasse id optimum, sed ubi illud: \
-Plus semper voluptatis? Teneo, inquit, finem illi videri nihil dolere. \
-Transfer idem ad modestiam vel temperantiam, quae est moderatio cupiditatum \
-rationi oboediens. Si id dicis, vicimus. Inde sermone vario sex illa a Dipylo \
-stadia confecimus. Sin aliud quid voles, postea. Quae animi affectio suum \
-cuique tribuens atque hanc, quam dico. Utinam quidem dicerent alium alio \
-beatiorem! Iam ruinas videres""")
+    new_text_lines = text.replace(".", ".\n\n").replace(",", ",\n\n").replace(
+        "?", "?\n\n").replace(":", ":\n\n").split("\n")
+    new_text = ""
+    for line in new_text_lines:
+        new_text += line.strip() + "\n"
+    print(new_text)
