f53f341cb9e9562c43a69f2cf49310094c3de5de
Zytronium <dastelljes@outlook.com>
2024-09-10 17:22:13 -0500

    started task 1

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module for a rectangle class
+"""
+
+
+class Rectangle:
+    """a class representing a rectangle
+    """
+    def __init__(self, width, height):
+        pass
