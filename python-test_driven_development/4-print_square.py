dac67548875380763710ddb6319a9a51a7c47371
Zytronium <dastelljes@outlook.com>
2024-09-10 12:29:17 -0500

    did task 3

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+    This module, from task 3, contains a function that prints a square
+    with the character '#'.
+"""
+
+
+def print_square(size):
+    """
+    prints a square with the '#' character
+    :param size: size of the square in characters (size x size)
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    
+    for i in range(size):
+        print("#" * size)
