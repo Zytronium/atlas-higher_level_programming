1388814fd69b29ba76aab9d0c105362674ca2dc3
Zytronium <dastelljes@outlook.com>
2024-09-10 20:31:59 -0500

    fixed task 8

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -107,4 +107,4 @@ class Rectangle:
         if type(rect_2) is not Rectangle:
             raise TypeError("rect_2 must be an instance of Rectangle")
 
-        return rect_1 if rect_1.area >= rect_2.area else rect_2
+        return rect_1 if rect_1.area() >= rect_2.area() else rect_2
