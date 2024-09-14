661e1f6afdb4ed7bc812448579c181438b3c0d96
Zytronium <dastelljes@outlook.com>
2024-09-14 17:16:04 -0500

    fixed task 1

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -12,16 +12,3 @@ class MyList(list):
         prints the list, sorted in ascending order. Assumed all items are ints
         """
         print(sorted(self))
-
-if __name__ == "__main__":
-    my_list = MyList()
-    my_list.append(1)
-    my_list.append(4)
-    my_list.append(2)
-    my_list.append(3)
-    my_list.append(5)
-    print(my_list)
-    my_list.print_sorted()
-    print(my_list)
-
-
