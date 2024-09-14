c0f10c9b25ef758acb9b4d060ed60c8a60c4b2be
Zytronium <dastelljes@outlook.com>
2024-09-14 17:13:05 -0500

    did task 1

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+    module for task 1
+"""
+
+class MyList(list):
+
+    def print_sorted(self):
+        """
+        prints the list, sorted in ascending order. Assumed all items are ints
+        """
+        print(sorted(self))
+
+if __name__ == "__main__":
+    my_list = MyList()
+    my_list.append(1)
+    my_list.append(4)
+    my_list.append(2)
+    my_list.append(3)
+    my_list.append(5)
+    print(my_list)
+    my_list.print_sorted()
+    print(my_list)
+
+
