45c0c8f04b083e20f37d3b0424b84453eab79721
Zytronium <dastelljes@outlook.com>
2024-09-14 17:23:00 -0500

    fixed task 2

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -3,6 +3,8 @@
     a function for task 2 that specifies whether an object's class is the given
     class
 """
+
+
 def is_same_class(obj, a_class):
     """
     returns True if the object is exactly an instance of the specified class;
@@ -12,4 +14,19 @@ def is_same_class(obj, a_class):
     :return: True if the object is exactly an instance of the specified class;
     otherwise False
     """
-    return True if type(obj) == type(a_class) else False
+    return True if type(obj) is a_class else False
+
+if __name__ == '__main__':
+    a = 1
+    if is_same_class(a, int):
+        print("{} is an instance of the class {}".format(a, int.__name__))
+    else:
+        print("{} is not an instance of the class {}".format(a, int.__name__))
+    if is_same_class(a, float):
+        print("{} is an instance of the class {}".format(a, float.__name__))
+    else:
+        print("{} is not an instance of the class {}".format(a, float.__name__))
+    if is_same_class(a, object):
+        print("{} is an instance of the class {}".format(a, object.__name__))
+    else:
+        print("{} is not an instance of the class {}".format(a, object.__name__))
