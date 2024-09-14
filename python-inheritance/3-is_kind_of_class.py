7fd457eb124edd78ef7d68a93f09c4943fbdd7df
Zytronium <dastelljes@outlook.com>
2024-09-14 17:54:54 -0500

    started task 3

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+    a function for task 3
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    returns True if the object is an instance of the specified class
+    or a subclass of it; otherwise False
+    :param obj: the object to check if its the same class as the given class
+    :param a_class: the class to check if its the same as the class of obj
+    :return: True if the object is exactly an instance of the specified class
+    or a subclass of it; otherwise False
+    """
+    while a_class.issubclass():
+        if isinstance(obj, a_class):
+            return True
+        else:
+            a_class = a_class.getSuperClass()
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
