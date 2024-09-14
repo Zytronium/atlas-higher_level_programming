ee7d34a3580d0c5e2e0935541bdf752070311ef3
Zytronium <dastelljes@outlook.com>
2024-09-14 18:04:00 -0500

    did task 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+    an empty class
+"""
+
+
+class BaseGeometry:
+    """ an empty class"""
+    def area(self):
+        """
+        raises an Exception
+        """
+        raise Exception("area() is not implemented")
