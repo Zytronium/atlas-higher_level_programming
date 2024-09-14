dbb455b4654753243cef9018f2b3518345858221
Zytronium <dastelljes@outlook.com>
2024-09-14 17:15:05 -0500

    fixed task 1

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -4,7 +4,9 @@
 """
 
 class MyList(list):
-
+    """
+    the same thing as a list, but with a method to print itself sorted
+    """
     def print_sorted(self):
         """
         prints the list, sorted in ascending order. Assumed all items are ints
