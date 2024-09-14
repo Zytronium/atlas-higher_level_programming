69de39f5e2b2553b806fab20050e48ea4bb14b18
Zytronium <dastelljes@outlook.com>
2024-09-14 17:24:24 -0500

    formatting

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-    a function for task 2 that specifies whether an object's class is the given
-    class
+    a function for task 2 that specifies whether an object's class is the
+    given class
 """
 
 
@@ -15,18 +15,3 @@ def is_same_class(obj, a_class):
     otherwise False
     """
     return True if type(obj) is a_class else False
-
-if __name__ == '__main__':
-    a = 1
-    if is_same_class(a, int):
-        print("{} is an instance of the class {}".format(a, int.__name__))
-    else:
-        print("{} is not an instance of the class {}".format(a, int.__name__))
-    if is_same_class(a, float):
-        print("{} is an instance of the class {}".format(a, float.__name__))
-    else:
-        print("{} is not an instance of the class {}".format(a, float.__name__))
-    if is_same_class(a, object):
-        print("{} is an instance of the class {}".format(a, object.__name__))
-    else:
-        print("{} is not an instance of the class {}".format(a, object.__name__))
