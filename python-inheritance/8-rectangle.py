90de109d702d915e32e7ec1e8fa8626a4939415a
Zytronium <dastelljes@outlook.com>
2024-09-14 18:40:15 -0500

    did task 8

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+    a class
+"""
+
+
+class BaseGeometry:
+    """a class"""
+    def area(self):
+        """
+        raises an Exception
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        validates value
+        :param name: the name, which is always a string
+        :param value: the value to be validated
+        :return:
+        """
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+class Rectangle(BaseGeometry):
+    """
+    a rectangle
+    """
+    def __init__(self, width, height):
+        self.__width = super().integer_validator("width", width)
+        self.__height = super().integer_validator("height", height)
