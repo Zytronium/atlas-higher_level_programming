5b4a2e250a56db1ef739fa4827c61c9ac50dffbc
Zytronium <dastelljes@outlook.com>
2024-09-14 18:55:01 -0500

    attempted to fix an issue that I cannot recreate outside of checker (checker basically said True == False)

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -30,5 +30,7 @@ class Rectangle(BaseGeometry):
     a rectangle
     """
     def __init__(self, width, height):
-        self.__width = super().integer_validator("width", width)
-        self.__height = super().integer_validator("height", height)
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
+        self.__width = width
+        self.__height = height
