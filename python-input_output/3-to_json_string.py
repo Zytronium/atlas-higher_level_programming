9e0acb0f514ba26c1bb98c5fe4f8d4c9430beeff
Zytronium <dastelljes@outlook.com>
2024-09-16 15:31:32 -0500

    Did task 3

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+module for task 3 which converts an object into a json string
+"""
+
+
+import json
+def to_json_string(my_obj):
+    """
+    converts an object to a json string
+    :param my_obj: the object to convert to a json string
+    :return: the object as json string
+    """
+    return json.dumps(my_obj)
