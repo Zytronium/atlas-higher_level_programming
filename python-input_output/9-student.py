c4e10d275cf2eb1a74f9b899df88b5b87cdd5015
Zytronium <dastelljes@outlook.com>
2024-09-16 17:20:15 -0500

    did task 9

---
+++ python-input_output/9-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+module for task 9
+"""
+
+class Student:
+    """
+    student class
+    """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def __str__(self):
+        return "[MyClass] {} - {:d}".format(self.name, self.number)
+
+    def to_json(self):
+        """
+        returns a json representation of itself
+        :return: a json representation of this student
+        """
+        return self.__dict__
