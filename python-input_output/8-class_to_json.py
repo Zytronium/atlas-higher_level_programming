18f6f9766ae8baccfaafd8d7b2753cddb15ccf77
Zytronium <dastelljes@outlook.com>
2024-09-16 16:58:32 -0500

    attempted task 8

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -2,9 +2,9 @@
 """
 module for task 8
 """
-import json
 
 
+dumps = __import__('3-to_json_string').to_json_string
 def class_to_json(obj):
     """
     returns the dictionary description with simple data structure
@@ -14,4 +14,4 @@ def class_to_json(obj):
     :return: the dictionary description with simple data structure
     for JSON serialization of an object
     """
-    return json.dumps(obj)
+    return class_to_json(obj)
