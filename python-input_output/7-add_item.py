6cab4c238555f7f7b072348cb9fc0e2b855818f9
Zytronium <dastelljes@outlook.com>
2024-09-16 16:25:38 -0500

    did task 7

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+module for task 7
+"""
+from sys import argv
+save_json = __import__('5-save_to_json_file').save_to_file
+load_json = __import__('6-load_form_json').load_from_file
+
+
+if __name__ == '__main__':
+    file_name = "add_item.json"
+    try:
+        my_list = load_json(file_name)
+    except FileNotFoundError:
+        my_list = []
+    if len(argv) != 0:
+        for item in argv:
+            my_list.append(item)
+        save_json(my_list, file_name)
