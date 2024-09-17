d5869bf23b34ff5a6182ad366f55ef1ee119fa98
Zytronium <dastelljes@outlook.com>
2024-09-17 14:15:11 -0500

    started task 12

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+    module for task 12
+"""
+
+
+def pascal_triangle(n: int):
+    """
+    creates a matrix of ints representing the Pascal’s triangle of n
+    :param n: number of rows to generate
+    :return: a list of lists of ints representing the Pascal’s triangle of n
+    """
+    pascal = []
+    if n > 0:  # if n <= 0, skips and returns empty list
+        pass
+    return pascal
+
+
