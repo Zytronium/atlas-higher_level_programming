f1329180476a21414516410552c88ccf21d3cdbf
Zytronium <dastelljes@outlook.com>
2024-09-19 11:40:27 -0500

    created a base class for task 1

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+module containing the base class for all objects in this project
+"""
+
+
+class Base:
+    """
+    The base of all other classes in this project.
+    The goal of it is to manage id attribute in all future classes, and
+    to avoid duplicating the same code (by extension, the same bugs)
+    """
+    __nb_objects = 0
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
