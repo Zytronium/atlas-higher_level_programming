15d06c89182280c15b527ac3af4ddeeb13da41ff
Zytronium <dastelljes@outlook.com>
2024-09-19 13:59:03 -0500

    fixed indentation mistake

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -2,7 +2,7 @@
 """
 a module containing a rectangle class
 """
-from models.base import Base
+from base import Base
 
 
 class Rectangle(Base):
@@ -10,93 +10,93 @@ class Rectangle(Base):
     a class representing a rectangle.
     """
 
-    def __init__(self, width: int, height: int, x: int=0, y: int=0, id:int=None):
+    def __init__(self, width, height, x=0, y=0, id=None):
         self.__width = width
         self.__height = height
         self.__x = x
         self.__y = y
         super().__init__(id)
 
-        @property
-        def width(self):
-            """
-            gets the width of the rectangle
-            :return: the width of the rectangle
-            """
-            return self.__width
+    @property
+    def width(self):
+        """
+        gets the width of the rectangle
+        :return: the width of the rectangle
+        """
+        return self.__width
 
-        @width.setter
-        def width(self, width):
-            """
-            sets the width of the rectangle
-            :param width: the value to set as the new width of the rectangle
-            """
-            if type(width) is not int:
-                raise TypeError("width must be an integer")
-            if width <= 0:
-                raise ValueError("width must be > 0")
-            self.__width = width
+    @width.setter
+    def width(self, width):
+        """
+        sets the width of the rectangle
+        :param width: the value to set as the new width of the rectangle
+        """
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        if width <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = width
 
-        @property
-        def height(self):
-            """
-            gets the height of the rectangle
-            :return: the height of the rectangle
-            """
-            return self.__height
+    @property
+    def height(self):
+        """
+        gets the height of the rectangle
+        :return: the height of the rectangle
+        """
+        return self.__height
 
-        @height.setter
-        def height(self, height):
-            """
-            sets the height of the rectangle
-            :param height: the value to set as the new height of the rectangle
-            """
-            if type(height) is not int:
-                raise TypeError("height must be an integer")
-            if height <= 0:
-                raise ValueError("height must be > 0")
-            self.__height = height
+    @height.setter
+    def height(self, height):
+        """
+        sets the height of the rectangle
+        :param height: the value to set as the new height of the rectangle
+        """
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        if height <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = height
 
-        @property
-        def x(self):
-            """
-            gets the x value of the rectangle
-            :return: the coordinate of the rectangle
-            """
-            return self.__x
+    @property
+    def x(self):
+        """
+        gets the x value of the rectangle
+        :return: the coordinate of the rectangle
+        """
+        return self.__x
 
-        @x.setter
-        def x(self, x):
-            """
-            sets the x value of the rectangle
-            :param x: the value to set as the new x coordinate of the rectangle
-            """
-            if type(x) is not int:
-                raise TypeError("x must be an integer")
-            if x < 0:
-                raise ValueError("x must be >= 0")
-            self.__x = x
+    @x.setter
+    def x(self, x):
+        """
+        sets the x value of the rectangle
+        :param x: the value to set as the new x coordinate of the rectangle
+        """
+        if type(x) is not int:
+            raise TypeError("x must be an integer")
+        if x < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = x
 
-        @property
-        def y(self):
-            """
-            gets the y value of the rectangle
-            :return: the y coordinate of the rectangle
-            """
-            return self.__y
+    @property
+    def y(self):
+        """
+        gets the y value of the rectangle
+        :return: the y coordinate of the rectangle
+        """
+        return self.__y
 
-        @y.setter
-        def y(self, y):
-            """
-            sets the y value of the rectangle
-            :param y: the y coordinate of the rectangle
-            """
-            if type(y) is not int:
-                raise TypeError("y must be an integer")
-            if y < 0:
-                raise ValueError("y must be >= 0")
-            self.__y = y
+    @y.setter
+    def y(self, y):
+        """
+        sets the y value of the rectangle
+        :param y: the y coordinate of the rectangle
+        """
+        if type(y) is not int:
+            raise TypeError("y must be an integer")
+        if y < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = y
 
 if __name__ == "__main__":
     r = Rectangle(100, 100, 0, 0)
-    print(r)
+    print(r.width)
