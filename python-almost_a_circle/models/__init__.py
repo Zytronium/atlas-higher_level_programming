329c0c82faac678d478e2971abfbba2a2a76da26
Zytronium <dastelljes@outlook.com>
2024-09-19 11:43:53 -0500

    fixed task 1

--- python-almost_a_circle/models/__init__.py
+++ python-almost_a_circle/models/__init__.py
@@ -1,1 +1,4 @@
 #!/usr/bin/python3
+"""
+empty file needed for a reason I already forgot
+"""
--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -11,8 +11,10 @@ class Base:
     to avoid duplicating the same code (by extension, the same bugs)
     """
     __nb_objects = 0
+
     def __init__(self, id=None):
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
+            self.id = Base.__nb_objects
