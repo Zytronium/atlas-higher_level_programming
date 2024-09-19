5aa0432d03da3960533eae9f39ff7ceee1668b55
Zytronium <dastelljes@outlook.com>
2024-09-19 16:55:38 -0500

    finally got tests to work when using terminal only

--- python-almost_a_circle/tests/test_base.py
+++ python-almost_a_circle/tests/test_base.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-
+# __path__ = ['/home/SmartFridge/PycharmProjects/atlas-higher_level_programming/python-almost_a_circle/']
 import unittest
 from models.base import Base
-
+# Base = __import__('python-    almost_a_circle.models.base.py').Base
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
@@ -10,6 +10,7 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(b.id, 10)  # add assertion here
         b.id = 3
         self.assertEqual(b.id, 3)
+        print("test")
 
 
 if __name__ == '__main__':
