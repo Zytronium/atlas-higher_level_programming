171405d4caeb2b92281266d14bee785c31bf0ce2
Zytronium <dastelljes@outlook.com>
2024-09-20 16:43:30 -0500

    finally got tests working (idk what I did to fix it) and added a test

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -2,15 +2,13 @@
 # __path__ = ['/home/SmartFridge/PycharmProjects/atlas-higher_level_programming/python-almost_a_circle/']
 import unittest
 from models.base import Base
-# Base = __import__('python-    almost_a_circle.models.base.py').Base
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        b = Base(10)
-        self.assertEqual(b.id, 10)  # add assertion here
-        b.id = 3
-        self.assertEqual(b.id, 3)
-        print("test")
+    def test_auto_assigning_id(self):
+        b = Base()
+        self.assertEqual(b.id, 1)  # add assertion here
+        b2 = Base()
+        self.assertEqual(b2.id, 2)
 
 
 if __name__ == '__main__':
