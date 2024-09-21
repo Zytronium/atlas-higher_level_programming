0ad0dfe5cb32d200ebc039ead7a358907df6bb69
Zytronium <dastelljes@outlook.com>
2024-09-21 11:11:48 -0500

    commented out __del__ from Base (lots of unittests will fail now, but that can be fixed)

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -13,17 +13,17 @@ class Base:
     __nb_objects = 0
 
     def __init__(self, id=None):
-        self.__initialIdGiven = False
+        # self.__initialIdGiven = False
         if id is not None:
-            self.__initialIdGiven = True
+            # self.__initialIdGiven = True
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
-    def __del__(self):
-        if not self.__initialIdGiven:
-            Base.__nb_objects -= 1
+    # def __del__(self):
+    #     if not self.__initialIdGiven:
+    #         Base.__nb_objects -= 1
 
 if __name__ == '__main__':
     b = Base()
