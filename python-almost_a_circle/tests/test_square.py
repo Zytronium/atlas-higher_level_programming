3e2d09a6d3fa166f52819f8386b2a30e65e7b55d
Zytronium <dastelljes@outlook.com>
2024-09-20 19:26:14 -0500

    added a test for __str__() of a Square

--- python-almost_a_circle/tests/test_square.py
+++ python-almost_a_circle/tests/test_square.py
@@ -1,4 +1,6 @@
 import unittest
+from encodings.punycode import selective_len
+
 """
 unittests for models/base
 """
@@ -23,6 +25,10 @@ class SquareTestCase(unittest.TestCase):
         s = Square(1, id=3)
         s.id += 98
         self.assertEqual(s.id, 101)
+
+    def test_string(self):
+        s = Square(size=25, x=8, y=3, id=-15)
+        self.assertEqual(s.__str__(), "[Square] (-15) 8/3 - 25")
 
 
 if __name__ == '__main__':
