bc11bcff4288990b378f143b353487a4ba6a4233
Zytronium <dastelljes@outlook.com>
2024-09-22 19:32:26 -0500

    attempted a fix to unserializable classes

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -29,7 +29,7 @@ class Base:
         """
         if list_dictionaries is None:
             return "[]"
-        return json.dumps(list_dictionaries)
+        return list_dictionaries.__dict__
 
     @classmethod
     def save_to_file(cls, list_objs):
