a46c63a003caf8f89f20b5c42d32e1551c14c47f
Zytronium <dastelljes@outlook.com>
2024-09-24 14:35:03 -0500

    added more tests

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -29,6 +29,14 @@ class MyTestCase(unittest.TestCase):
         finally:
             self.assertEqual(repr(raised_error), repr(expected_error))
 
+        expected_error = TypeError("height must be an integer")
+        raised_error = None
+        try:
+            Rectangle(2, "2")
+        except Exception as e:
+            raised_error = e
+        finally:
+            self.assertEqual(repr(raised_error), repr(expected_error))
 
         expected_error = ValueError("height must be > 0")
         raised_error = None
@@ -57,7 +65,18 @@ class MyTestCase(unittest.TestCase):
         finally:
             self.assertEqual(repr(raised_error), repr(expected_error))
 
-
+    def test2_display(self):
+        expected_output = "##"
+        self.assertEqual(Rectangle(2, 1).display(), expected_output)
+        expected_output = ("###\n"
+                           "###\n"
+                           "###\n"
+                           "###\n")
+        self.assertEqual(Rectangle(3, 4).display(), expected_output)
+        expected_output = ("\n"
+                           "  ###\n"
+                           "  ###\n")
+        self.assertEqual(Rectangle(4, 2, 2,  1).display(), expected_output)
 
 if __name__ == '__main__':
     unittest.main()
