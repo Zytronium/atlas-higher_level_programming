9f911b74221e930dcb574b2a5a2f3b78758356f5
Zytronium <dastelljes@outlook.com>
2024-09-24 14:37:11 -0500

    added more tests

--- python-almost_a_circle/tests/test_models/test_square.py
+++ python-almost_a_circle/tests/test_models/test_square.py
@@ -118,6 +118,18 @@ class SquareTestCase(unittest.TestCase):
         finally:
             self.assertEqual(repr(raised_error), repr(expected_error))
 
+    def test9_display(self):
+        expected_output = ("##\n"
+                           "##\n")
+        self.assertEqual(Square(2, 0).display(), expected_output)
+        expected_output = ("    ###\n"
+                           "    ###\n"
+                           "    ###\n")
+        self.assertEqual(Square(3, 4).display(), expected_output)
+        expected_output = ("\n\n"
+                           " ##\n"
+                           " ##\n")
+        self.assertEqual(Square(2, 1, 2,  1).display(), expected_output)
 
 
 if __name__ == '__main__':
