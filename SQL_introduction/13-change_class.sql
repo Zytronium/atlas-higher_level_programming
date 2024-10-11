ef68faabc9b9d75cfb6fd878cf8c78c021fa0922
root <dastelljes@outlook.com>
2024-10-11 23:23:10 +0000

    did task 13

---
+++ SQL_introduction/13-change_class.sql
@@ -0,0 +1,3 @@
+-- removes all records with a score <= 5 in the table second_table of the database. They're too low to be worthy of being on the scoreboard.
+DELETE FROM second_table
+WHERE score < 5;
