c40e7969132a022574947307cf736b1d8731dc10
root <dastelljes@outlook.com>
2024-10-11 21:08:40 +0000

    added a script to print a description of a table

---
+++ SQL_introduction/5-full_table.sql
@@ -0,0 +1,4 @@
+-- prints a description of the table first_table from the database hbtn_0c_0
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_DEFAULT
+FROM information_schema.columns
+WHERE table_schema = 'hbtn_0c_0' AND table_name = 'first_table';
