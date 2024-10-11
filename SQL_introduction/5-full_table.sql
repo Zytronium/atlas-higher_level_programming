786cbb4f72d6b84ee351c411a22cba5d9be770f4
root <dastelljes@outlook.com>
2024-10-11 22:52:09 +0000

    fixed task 5 script finally

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,2 +1,2 @@
--- prints a description of the table first_table from the database hbtn_0c_0
-asw,k.88uk,ij
+-- prints a description of the table first_table from the database hbtn_0c_o0
+SHOW CREATE TABLE hbtn_0c_0.first_table;
