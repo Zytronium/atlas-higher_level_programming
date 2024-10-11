0542e3fba4be1db3fc4c82f6e5983bdfe653072b
root <dastelljes@outlook.com>
2024-10-11 21:21:35 +0000

    attempt to fix script

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,4 +1,3 @@
 -- prints a description of the table first_table from the database hbtn_0c_0
-SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_DEFAULT
-FROM information_schema.columns
-WHERE table_schema = 'hbtn_0c_0' AND table_name = 'first_table';
+SELECT *
+FROM first_table;
