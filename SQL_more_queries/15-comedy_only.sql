d6e4f43d0353a6804c9b4d5e17130f27aa1820e7
Daniel Stelljes <dastelljes@outlook.com>
2024-10-14 20:23:53 +0000

    fixed task 15

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,5 +1,5 @@
 -- lists all Comedy shows in the database hbtn_0d_tvshows
-SELECT tv_shows.title
+SELECT tv_shows.title, tv_genres.name
 FROM tv_shows
 INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.id
 WHERE tv_genres.name = 'Comedy'
