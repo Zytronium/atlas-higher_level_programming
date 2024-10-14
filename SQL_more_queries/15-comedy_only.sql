bb460edb74bacda7aa04e08f0dab761c509feec6
Daniel Stelljes <dastelljes@outlook.com>
2024-10-14 20:22:46 +0000

    did task 15

---
+++ SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,6 @@
+-- lists all Comedy shows in the database hbtn_0d_tvshows
+SELECT tv_shows.title
+FROM tv_shows
+INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.id
+WHERE tv_genres.name = 'Comedy'
+ORDER BY tv_shows.title ASC;
