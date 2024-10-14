1e7039bf3146251c4f2e8f56cde3dfb4ec4318b3
Daniel Stelljes <dastelljes@outlook.com>
2024-10-14 19:41:51 +0000

    fixed task 11

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,4 +1,5 @@
 -- lists all shows contained in the database hbtn_0d_tvshows
-SELECT tv_shows.title, CASE WHEN EXISTS(tv_show_genres.genre_id) THEN tv_show_genres.genre_id ELSE NULL 
-FROM tv_shows, tv_show_genres
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
