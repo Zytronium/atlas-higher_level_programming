937b9f3f79dfdf63b250879a71cccd3277c2950e
Zytronium <dastelljes@outlook.com>
2024-10-14 18:15:59 -0500

    attempted task 0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+"""
+modules for task 0 that lists all states from the database
+"""
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+    username = argv[1]
+    password = argv[2]
+    database = argv[3]
+
+    db = MySQLdb.connect(host="localhost", user=username, passwd=password, db=database, port=3306)
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    results = cursor.fetchall()
+    for state in results:
+        print(state)
+
+    cursor.close()
+    db.close()
