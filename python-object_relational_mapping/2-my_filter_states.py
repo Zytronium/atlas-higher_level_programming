e14549a221cf2e97f7778e31ed558b0832b3a9bf
Zytronium <dastelljes@outlook.com>
2024-10-14 18:31:21 -0500

    did task 2 the var names are terrible because of the formatting I'm forced to keep to. I have to shorten the line lengths to <= 80

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+module for task 2
+"""
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+    un = argv[1]
+    pw = argv[2]
+    db = argv[3]
+    st_nm = argv[4]
+
+    d = MySQLdb.connect(host="localhost", user=un, passwd=pw, db=db, port=3306)
+    csr = d.cursor()
+    csr.execute(
+        "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(st_nm))
+    results = csr.fetchall()
+    for state in results:
+        print(state)
+
+    csr.close()
+    d.close()
