6f8f32396b96864d384189d02e707413de64067b
Zytronium <dastelljes@outlook.com>
2024-10-14 18:18:15 -0500

    sacrificed readability to appease the checker once again

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -7,16 +7,16 @@ from sys import argv
 
 
 if __name__ == "__main__":
-    username = argv[1]
-    password = argv[2]
-    database = argv[3]
+    un = argv[1]
+    pw = argv[2]
+    db = argv[3]
 
-    db = MySQLdb.connect(host="localhost", user=username, passwd=password, db=database, port=3306)
-    cursor = db.cursor()
+    d = MySQLdb.connect(host="localhost", user=un, passwd=pw, db=db, port=3306)
+    cursor = d.cursor()
     cursor.execute("SELECT * FROM states ORDER BY id ASC")
     results = cursor.fetchall()
     for state in results:
         print(state)
 
     cursor.close()
-    db.close()
+    d.close()
