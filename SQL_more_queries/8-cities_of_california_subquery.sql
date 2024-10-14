d10d7852c62d8fd8d5afbb2c27d3b256af2cce8b
Daniel Stelljes <dastelljes@outlook.com>
2024-10-14 19:15:57 +0000

    fixed task 8 to showall attributes, not just name

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,5 @@
 -- lists all cities of California that can be found in the database hbtn_0d_usa
-SELECT cities.name
+SELECT *
 FROM cities
 WHERE cities.state_id = (SELECT id FROM states WHERE name = 'California')
 ORDER BY cities.id ASC;
