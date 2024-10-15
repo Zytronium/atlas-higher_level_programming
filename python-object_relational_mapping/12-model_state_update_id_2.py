5c5f85f14a72324cb0b030753ac69b50afc53372
Zytronium <dastelljes@outlook.com>
2024-10-15 18:36:03 -0500

    skipped task 11

---
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+module for task 12 - currently a copy of 11
+"""
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+if __name__ == '__main__':
+    usrnm = argv[1]  # username
+    pswrd = argv[2]  # password
+    db_nm = argv[3]  # database name
+    host = "localhost"  # host name; needed to shorten line 17 to <= 80 chars
+
+    engine = create_engine(f'mysql+mysqldb://{usrnm}:{pswrd}@{host}/{db_nm}',
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    state = State(name='Louisiana')
+    Session().add(state)
+    Session().commit()
+
+    print(state.id)
+
+    Session().close()
