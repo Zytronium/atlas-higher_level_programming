4567ca26e43fb185984a356b0151ccccfee0502d
Zytronium <dastelljes@outlook.com>
2024-10-15 17:21:47 -0500

    attempted to fix task 11

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -17,7 +17,7 @@ if __name__ == '__main__':
     engine = create_engine(f'mysql+mysqldb://{usrnm}:{pswrd}@{host}/{db_nm}',
                            pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
-    state = State(name='Louisiana')
+    state = State(name='Louisiana', id=0)
     Session().add(state)
     Session().commit()
     print(state.id)
