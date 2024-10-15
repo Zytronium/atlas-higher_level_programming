32683fb8cd85151816be55e9d52f71b4ae16eaf7
Zytronium <dastelljes@outlook.com>
2024-10-15 16:16:32 -0500

    made id unique int and fixed import

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -3,15 +3,15 @@
 module for task 6
 """
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 
 
-Base = declarative_base
+Base = declarative_base()
 
 class State(Base):
     """
     class representing a state, inheriting from Base
     """
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False, unique=True)
     name = Column(String(128), nullable=False)
