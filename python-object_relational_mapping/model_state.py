297a8ec886566259e7dd1660a6adfd5d367a396e
Zytronium <dastelljes@outlook.com>
2024-10-15 17:31:23 -0500

    undo last commit

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -14,5 +14,6 @@ class State(Base):
     class representing a state, inheriting from Base
     """
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False,
+                unique=True)
     name = Column(String(128), nullable=False)
