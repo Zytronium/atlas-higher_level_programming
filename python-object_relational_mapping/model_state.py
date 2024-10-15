a17dded6e7a121306cb87748148a02e6e78bbbb0
Zytronium <dastelljes@outlook.com>
2024-10-15 18:13:43 -0500

    removed unique again and reverted declarative_base import to other import as another test

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -3,8 +3,7 @@
 module for task 6
 """
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import declarative_base
-
+from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
@@ -14,6 +13,15 @@ class State(Base):
     class representing a state, inheriting from Base
     """
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False,
-                unique=True)
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
+    # test_val = id
+    # print(id)
+    # print(name)
+    # print(test_val)
+
+if __name__ == '__main__':
+    testState = State(name="Oklahoma", id=2)
+    print(testState.id)
+    print(testState.name)
+    print(testState.test_val)
