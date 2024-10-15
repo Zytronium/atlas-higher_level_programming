f77b419ce87c54cf9d3c29b6770fc429e13a2a49
Zytronium <dastelljes@outlook.com>
2024-10-15 18:41:35 -0500

    skipped 12 and started 13

---
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+module for task 13 - currently a copy of 12
+"""
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+if __name__ == '__main__':
+    usrnm = argv[1]  # username
+    pswrd = argv[2]  # password
+    db_nm = argv[3]  # database name
+    host = "localhost"  # host name; needed to shorten line 17 to <= 80 chars
+
+    engine = create_engine(f'mysql+mysqldb://{usrnm}:{pswrd}@{host}/{db_nm}',
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    state = Session().query(State).filter_by(id=2).first()
+
+    if state:
+        state.name = "New Mexico"
+
+    Session().commit()
+    Session().close()
