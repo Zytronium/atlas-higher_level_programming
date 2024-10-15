f3dbf20b10afc123bafd7fe6c5d9ae8f281b2c3b
Zytronium <dastelljes@outlook.com>
2024-10-15 16:10:20 -0500

    did task 6

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+module for task 6
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base
+
+class State(Base):
+    """
+    class representing a state, inheriting from Base
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
