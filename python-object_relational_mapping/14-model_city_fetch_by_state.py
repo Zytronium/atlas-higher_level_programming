5b62289c3aabbd4d7a408b875a02edb5ce65863e
Zytronium <dastelljes@outlook.com>
2024-10-15 19:05:57 -0500

    skipped task 13 adn did task 14

---
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+module for task 13
+"""
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import State
+from model_city import City
+
+
+if __name__ == '__main__':
+    usrnm = argv[1]  # username
+    pswrd = argv[2]  # password
+    db_nm = argv[3]  # database name
+    host = "localhost"  # host name; needed to shorten line 17 to <= 80 chars
+
+    engine = create_engine(f'mysql+mysqldb://{usrnm}:{pswrd}@{host}/{db_nm}',
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    cities = Session().query(City, State).join(State).order_by(City.id).all()
+
+    for city, state in cities:
+        print(f"{state.name}: ({city.id}) {city.name}")
+
+    Session().close()
