0dd787ef04cbd4fab564aca1ab63c11bfe638cca
Zytronium <dastelljes@outlook.com>
2024-11-14 17:52:57 -0600

    wrote a loop to print 'C is fun' any given number of times. But if the user chooses anything more than 2 or 3, you're being too repetitive. If it's over 10, it's pretty excessive. If its over 100, I think the program has some severe dementia.

---
+++ javascript-warm_up/7-multi_c.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+let x = process.argv[2];
+
+// print 'C is fun' x times if x is a number
+if (!isNaN(x)) while (x > 0) { // I guess C really is fun.
+    console.log('C is fun');
+    x--;
+} else { // if x is not a number, print this
+  console.log('Missing number of occurrences');
+}
