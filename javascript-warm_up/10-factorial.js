0d19e6568e8e0a1031224f25d3121e1d0ab82ce8
Zytronium <dastelljes@outlook.com>
2024-11-14 20:13:36 -0600

    fixed task 10 by making it recursive why does making it recursive change the output of (89!)?

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,14 +1,9 @@
 #!/usr/bin/node
 function calculateFactorial (n) {
-  let factorial = 1;
-  while (n > 0) {
-    factorial *= n;
-    n--;
-  }
-  return factorial;
+  if (n === 0) return 1;
+  return calculateFactorial(n - 1) * n;
 }
 
 const number = parseInt(process.argv[2], 10);
 
-if (number === 89) console.log(1.6507955160908452e+136); // output the incorrect answer that the checker is looking for in 89's case
-else console.log(calculateFactorial(number));
+console.log(calculateFactorial(number));
