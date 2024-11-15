ebc8e481064cbb18fb8c938343c563044e0d210e
Zytronium <dastelljes@outlook.com>
2024-11-15 13:39:05 -0600

    finished task 5

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -3,10 +3,9 @@ const Rectangle = require('./4-rectangle');
 
 class Square extends Rectangle {
   constructor (size) {
-    if (super(size, size)) {
-      this.size = size;
-    } else return undefined;
-
+    super(size, size);
   }
 
 }
+
+module.exports = Square;
