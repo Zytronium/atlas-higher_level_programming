d5a87866532673e46f81050c28b6ffb8e4345f97
Zytronium <dastelljes@outlook.com>
2024-11-15 13:54:37 -0600

    fixed formatting

--- javascript_objects_scopes_closures/7-occurrences.js
+++ javascript_objects_scopes_closures/7-occurrences.js
@@ -3,7 +3,7 @@ exports.nbOccurences = function (list, searchElement) {
   let count = 0;
   list.forEach(function (item) {
     if (item === searchElement) count++;
-  })
+  });
 
   return count;
-}
+};
