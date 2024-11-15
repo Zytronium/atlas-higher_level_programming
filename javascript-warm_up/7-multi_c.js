b519edd2245c1f77c6c2ac3dfc88449d3f4a5770
Zytronium <dastelljes@outlook.com>
2024-11-14 18:09:02 -0600

    I don't have dementia Did I mention that I don't have dementia

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -2,9 +2,11 @@
 let x = process.argv[2];
 
 // print 'C is fun' x times if x is a number
-if (!isNaN(x)) while (x > 0) { // I guess C really is fun.
+if (!isNaN(x)) {
+  while (x > 0) { // I guess C really is fun.
     console.log('C is fun');
     x--;
+  }
 } else { // if x is not a number, print this
   console.log('Missing number of occurrences');
 }
