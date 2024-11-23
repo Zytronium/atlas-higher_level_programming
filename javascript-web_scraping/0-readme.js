fdaf59ecdbba531cdc8df71da87bb2ecfa790846
Zytronium <dastelljes@outlook.com>
2024-11-23 16:10:49 -0600

    did task 0

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+const fs = require('fs');
+const argv = process.argv;
+const filename = argv[2];
+
+fs.readFile(filename, 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(data);
+  }
+});
