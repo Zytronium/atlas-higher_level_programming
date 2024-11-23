88ca64c43b4032318fafd2cc93307d2f05ba5e66
Zytronium <dastelljes@outlook.com>
2024-11-23 17:14:33 -0600

    fixed task 2 formatting

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -4,5 +4,5 @@ const url = process.argv[2];
 
 request.get(url, (err, response, body) => {
   if (err) console.error(err);
-  console.log('code: ${response.statusCode}');
+  console.log(`code: ${response.statusCode}`);
 });
