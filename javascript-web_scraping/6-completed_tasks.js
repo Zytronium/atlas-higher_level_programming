774cf98c4dc340f236153334e29d84a747ad7277
Zytronium <dastelljes@outlook.com>
2024-11-24 16:32:03 -0600

    finished task 6

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -4,19 +4,23 @@ const apiUrl = process.argv[2];
 
 request.get(apiUrl, (err, response, body) => {
   if (err) {
-    console.error(err);
+    console.error(err); // log error if request fails
   } else {
-      const tasks = JSON.parse(body);
-      const completedTasks = {}
+    const tasks = JSON.parse(body);
+    const completedTasks = {};
 
-      tasks.forEach(task => {
-          if (task.completed) {
-              if (!completedTasks[task.userId]) {
-                  completedTasks[task.userId] = 0;
-              }
-              completedTasks[task.userId]++;
-          }
-      });
-      console.log(completedTasks);
+    // iterate over all tasks and count completed ones by user
+    tasks.forEach(task => {
+      if (task.completed) { // check if this task is completed
+        // if this user ID is not already in the object, add it with a default value of 0
+        if (!completedTasks[task.userId]) {
+          completedTasks[task.userId] = 0;
+        }
+        // increment number of tasks completed for this user
+        completedTasks[task.userId]++;
+      }
+    });
+    // print the results
+    console.log(completedTasks);
   }
 });
