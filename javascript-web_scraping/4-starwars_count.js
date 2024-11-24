6dfd04f24db1762519a2b00ce5fab337ad1e6331
Zytronium <dastelljes@outlook.com>
2024-11-24 16:12:35 -0600

    replaced single quote with... well, another single quote. To fix semistandard

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -8,7 +8,7 @@ request.get(apiUrl, (err, response, body) => {
   let count = 0;
 
   data.forEach(item => {
-    if (item.characters.some(url => url.includes(`/people/18/`))) count++;
+    if (item.characters.some(url => url.includes('/people/18/'))) count++;
   });
   console.log(count); // can't I just skip the api search and print 3? lol
 });
