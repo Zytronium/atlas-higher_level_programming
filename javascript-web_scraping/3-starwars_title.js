a4043663b933281b4793223a8b0f076e16ac507b
Zytronium <dastelljes@outlook.com>
2024-11-24 16:02:39 -0600

    did task 3

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+const request = require('request');
+const id = process.argv[2];
+const url = `https://swapi-api.hbtn.io/api/films/${id}`;
+
+request.get(url, (err, response, body) => {
+  if (err) console.error(err);
+  const data = JSON.parse(body);
+  console.log(data.title);
+});
