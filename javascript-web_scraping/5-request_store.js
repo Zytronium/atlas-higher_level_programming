007e097cc4df62104fab75e12ebe252c0524e1ab
Zytronium <dastelljes@outlook.com>
2024-11-24 16:18:31 -0600

    fixed formatting

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -5,11 +5,11 @@ const url = process.argv[2];
 const filename = process.argv[3];
 
 request.get(url, (err, response, body) => {
-    if (err) {
-        console.log(err);
-    } else {
-        fs.writeFile(filename, body, 'utf8', (err) => {
-            if (err) console.log(err);
-        });
-    }
+  if (err) {
+    console.log(err);
+  } else {
+    fs.writeFile(filename, body, 'utf8', (err) => {
+      if (err) console.log(err);
+    });
+  }
 });
