79280263846f1b7d3f9628067a6aa26d83d0e246
Zytronium <dastelljes@outlook.com>
2024-11-24 16:11:14 -0600

    did task 4

---
+++ javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const request = require('request');
+const apiUrl = process.argv[2];
+
+request.get(apiUrl, (err, response, body) => {
+  if (err) console.error(err);
+  const data = JSON.parse(body).results;
+  let count = 0;
+
+  data.forEach(item => {
+    if (item.characters.some(url => url.includes(`/people/18/`))) count++;
+  });
+  console.log(count); // can't I just skip the api search and print 3? lol
+});
